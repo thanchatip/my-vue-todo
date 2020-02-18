853f514218f29a609554f619797b1f97dff62976
thanchatip <thanchatip.coe@gmail.com>
2020-02-18 16:53:39 +0700

    :construction: add move up-down function

--- myTodo.vue
+++ myTodo.vue
@@ -1,54 +1,60 @@
 <template>
     <div id="app" class="container">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a v-on:click="loadTodo()" class="navbar-brand" href="#"><h4> MyTodo </h4></a>
             <button class="navbar-toggler" type="button" 
                 data-toggle="collapse" data-target="#navbarNav" 
                 aria-controls="navbarNav" aria-expanded="false" 
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                 <li class="nav-item active">
-                    <a @click="loadTodo() , showCreate = 0 , showTodo = 1" class="nav-link" href="#"> Todo List <span class="sr-only">(current)</span></a>
+                    <a v-on:click="loadTodo() , showCreate = false , showTodo = true" class="nav-link" href="#"> Todo List <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a @click="showTodo =0 , showCreate = 1" class="nav-link" href="#"> Create </a>
+                    <a v-on:click="showTodo = false , showCreate = true, isEmpty = false" class="nav-link" href="#"> Create <span class="sr-only">(current)</span></a>
                 </li>
                 </ul>
             </div>
         </nav>
+
         <!--create page-->
-        <div v-if="showCreate" class="card">
-            <div class="card-body">
-                <h3 class="card-title">Add New Todo</h3>
-                <label for="bookTitle">Input todo</label><br>
-                <input type="text" v-model="newTask" class="form-control">
-                <label for="bookTitle"><small>* Required</small></label><br>
-                <label for="bookTitle">Description</label><br>
-                <input type="text" v-model="newDescription" class="form-control"><br>
-                <button type="button" class="btn btn-primary" v-on:click="loadTodo()"> Cancel </button>&nbsp;
-                <button type="button" class="btn btn-primary" v-on:click="saveTodo()"> Save </button> 
+        <div v-if="showCreate" >
+            <div v-if="isEmpty" class="alert alert-danger" role="alert">
+                Please Enter Todo !!!
             </div>
+
+            <div class="form-group">
+                <label for="todoTitle"> Input todo </label><br>
+                <input v-model="newTask" v-on:click="isEmpty = false" type="text" class="form-control">
+                <label for="todoTitle" class="form-text text-muted"><small>* Required</small></label><br>
+            </div>
+            <div class="form-group">    
+                <label for="todoTitle"> Description </label><br>
+                <input v-model="newDescription" type="text" class="form-control"><br>
+            </div>
+                <button type="button" class="btn btn-primary" v-on:click="loadTodo(), showCreate = false , showTodo = true"> Cancel </button>&nbsp;
+                <button type="button" class="btn btn-primary" v-if="isEmpty !== true" v-on:click="saveTodo(), showCreate = false , showTodo = true"> Save </button>
+                <button type="button" class="btn btn-primary" v-if="isEmpty" v-on:click="showCreate = true , showTodo = false , isEmpty = true"> Save </button>   
         </div>
         <br>
         <!--home page-->
-        <div v-if="showTodo" class="card">
-            <div class="card-body">
-                <h3 class="card-title">Todo List</h3>
-                    <div class="card" v-for="(todo) in todos" v-bind:key="todo.id">
-                    <div class="card-body" > 
-                        <h5>{{todo.task}}</h5>
-                        <h6>{{todo.description}}</h6>
-                        <button class="btn btn-primary" > Edit </button>&nbsp;
-                        <button class="btn btn-danger" v-on:click="removeTodo(todo.id)"> Delete </button>     
-                    </div>
-                    <br> 
-                    </div>
+        <div v-if="showTodo">
+            <div  class="card mb-2" v-for="(todo , index) in todos" v-bind:key="todo.id">
+                <div class="card-body" > 
+                    <h5 class="card-title">{{ todo.task }}</h5>
+                    <p class="card-text">{{ todo.description }}</p>
+                    <button v-on:click=" todoRef = todo.id ,showCreate = true , showTodo = false , isEdit = true" class="btn btn-primary" > Edit </button>&nbsp;
+                    <button class="btn btn-danger" v-on:click="removeTodo(todo.id)"> Delete </button>&nbsp;
+                    <button v-if="index !== 0" type="button" class="btn btn-outline-info" v-on:click="moveUp(index)"> Up </button>&nbsp;
+                    <button v-if="index !== todos.length-1" type="button" class="btn btn-outline-info" v-on:click="moveDown(index)"> Down </button><br> 
             </div>
         </div>
     </div>
+</div>
 </template>
 
 <!--firebase config-->
@@ -79,21 +85,30 @@ export default {
                 newTask:'',
                 newDescription:'',
                 removeTask:'',
-                showTodo: false,
-                showCreate: false
+                showTodo: true,
+                showCreate: false,
+                isEdit: false,
+                isEmpty:false,
+                docRef:''
             }
+    },
+    mounted() {
+        this.loadTodo();
     },
     firestore() {
             return {
                 todos:db.collection('todos'),
             }
-        },
+    },
     methods: {
         saveTodo() {
-                db.collection('todos').add({
-                    task : this.newTask,
-                    description : this.newDescription,
-                });
+            let todo = [];
+                if(this.newTask && this.newDescription != false){
+                    db.collection('todos').add({
+                        task : this.newTask,
+                        description : this.newDescription,
+                    }).then(this.newTask = '',this.newDescription ='');
+                }
                 this.loadTodo(); 
             },
             loadTodo() {
@@ -110,13 +125,34 @@ export default {
                 });
                 this.todos = todolist;
             },
+            editTodo(collectionID){
+                db.collection('todos').doc(collectionID).update({ 
+                    task: newTodo
+                    })
+                .then(() => {
+                    console.log('updated!')
+                });
+            },
             removeTodo(collectionID) {
                 db.collection('todos').doc(collectionID).delete().then(function() {
                     }).catch(function(error) {
                         console.error("Error removing document: ", error);
                         });  
                 this.loadTodo();        
-                    }         
+                    },
+            moveUp(index) {
+                if (index === 0) { return }
+                let todo = this.todos[index]
+                this.todos.splice(index, 1)
+                console.log(todo)
+                this.todos.splice(index-1, 0, todo)
+        },
+            moveDown(index) {
+                if (index == this.todos.length - 1) { return }
+                let todo = this.todos[index]
+                this.todos.splice(index, 1)
+                this.todos.splice(index+1, 0, todo)
+        }         
         }
         
     }
